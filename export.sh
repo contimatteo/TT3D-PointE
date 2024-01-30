3ac69bf935987027ce9f45f0ae06712dcafe7154
Matteo Conti <contimatteo1505@gmail.com>
2024-01-30 19:43:00 +0100

    WIP

---
+++ export.sh
@@ -0,0 +1,8 @@
+###
+### Unibo CV-LAB Server run script 
+###
+
+exit 0
+
+CUDA_VISIBLE_DEVICES=2 python3 tt3d_export.py \
+  --source-path /media/data2/mconti/TT3D/models/PointE/outputs/
---
+++ generate.sh
@@ -0,0 +1,9 @@
+###
+### Unibo CV-LAB Server run script 
+###
+
+exit 0
+
+CUDA_VISIBLE_DEVICES=2 python3 tt3d_generate.py \
+  --prompt-file /media/data2/mconti/TT3D/prompts/test.v1.n2.txt \
+  --out-path /media/data2/mconti/TT3D/models/PointE/outputs/
